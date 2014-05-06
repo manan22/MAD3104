a27791887f0687309d558055eb79078fbec9948c
PManan <mananpandya04@yahoo.com>
2014-05-06 13:59:41 -0400

    Project Start

---
+++ DAY2LESSON/DAY2LESSON/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DAY2LESSON
+//
+//  Created by macadmin on 2014-05-06.
+//  Copyright (c) 2014 MANAN PANDYA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
